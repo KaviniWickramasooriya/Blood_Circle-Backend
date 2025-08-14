530be171689c19f3b001bfaadf792ea1029bb7ef
Iresh Eranga <iresheranga2020al@gmail.com>
2025-08-14 21:03:34 +0530

    Update EventOrganiser.js
    Update event organiser model

--- user-service/src/models/EventOrganiser.js
+++ user-service/src/models/EventOrganiser.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
   const EventOrganiser = sequelize.define('EventOrganiser', {
@@ -9,13 +10,76 @@ module.exports = (sequelize) => {
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Name is required' },
+        len: { args: [2, 100], msg: 'Name must be between 2 and 100 characters' }
+      }
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: {
+        msg: 'Email already exists'
+      },
+      validate: {
+        isEmail: { msg: 'Invalid email format' },
+        notEmpty: { msg: 'Email is required' }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Password is required' },
+        len: { args: [8, 255], msg: 'Password must be at least 8 characters long' }
+      }
+    },
+    nic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'NIC number is required' },
+        len: { args: [8, 255], msg: 'NIC number must be at least 8 characters long' }
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    roleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Roles',
+        key: 'id'
+      }
+    },
+    telephoneNo: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  });
+  },{
+      hooks: {
+        beforeCreate: async (eventOrganiser) => {
+          try {
+            const salt = await bcrypt.genSalt(10);
+            eventOrganiser.password = await bcrypt.hash(eventOrganiser.password, salt);
+          } catch (error) {
+            throw new Error('Error hashing password');
+          }
+        },
+        beforeUpdate: async (eventOrganiser) => {
+          if (eventOrganiser.changed('password')) {
+            try {
+              const salt = await bcrypt.genSalt(10);
+              eventOrganiser.password = await bcrypt.hash(eventOrganiser.password, salt);
+            } catch (error) {
+              throw new Error('Error hashing password');
+            }
+          }
+        }
+      }
+    });
   return EventOrganiser;
 };
