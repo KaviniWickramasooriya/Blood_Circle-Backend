e15f2c80a19972245e8a0bedfca91175c2642f5a
Iresh Eranga <iresheranga2020al@gmail.com>
2025-08-16 11:21:41 +0530

    get donor count

--- user-service/src/routes/donorRoutes.js
+++ user-service/src/routes/donorRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const donorController = require("../controllers/donorController");
 
 router.post('/register', donorController.registerDonor);
+router.get('/count', donorController.getDonorCount);
 
 module.exports = router;
